785d85700f82bdf112c4301da89af90349aac359
MatsevkoVM <matevko.v.m@gmail.com>
2023-09-05 11:04:19 +0400

    Fixed collaborator related bugs.

--- src/main/java/com/todolist/dto/ToDoResponse.java
+++ src/main/java/com/todolist/dto/ToDoResponse.java
@@ -6,6 +6,7 @@ import com.todolist.model.ToDo;
 import lombok.Value;
 
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,8 +25,12 @@ public class ToDoResponse {
         title = toDo.getTitle();
         createdAt = toDo.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy/MM/dd hh:mm"));
         owner = toDo.getOwner().getFirstName() + " " + toDo.getOwner().getLastName();
-        collaborators = toDo.getCollaborators().stream()
-                .map(c -> c.getFirstName() + " " + c.getLastName())
-                .collect(Collectors.toList());
+        if (toDo.getCollaborators() != null) {
+            collaborators = toDo.getCollaborators().stream()
+                    .map(c -> c.getFirstName() + " " + c.getLastName())
+                    .collect(Collectors.toList());
+        } else {
+            collaborators = new ArrayList<>();
+        }
     }
 }
